1d1ab6dc9c2a8940e255740aa8675ba8c66a4f41
dharmayuset <dharmakemo@gmail.com>
2022-07-02 14:38:32 +0700

    done variable section

--- variable-dec/main.go
+++ variable-dec/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	//var card string = "ace of spade"
 	//tes
-	card := "ace of spade"
+	card := "ace of spades"
+	//new assign
+	card = "ace of diamonds"
 	fmt.Println(card)
 }
