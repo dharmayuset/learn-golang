5219a25a3bdcddb83c985ce5e4beb5a1f155487b
dharmayuset <dharmakemo@gmail.com>
2022-07-05 05:23:52 +0700

    end of slice

---
+++ oop-approach/deck.go
@@ -0,0 +1,5 @@
+package main
+
+// create new deck type of 'deck'
+// which is a slice of strings
+type deck []string
--- oop-approach/main.go
+++ oop-approach/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of Spades", newCard()}
+	cards := deck{"Ace of Spades", newCard()}
 	cards = append(cards, "Six of Diamonds")
 	// its example slice and for in go language
 	for i, card := range cards {
